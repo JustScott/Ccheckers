e1152f7adf7796721a02f5d072f55d55af5cd65f
JustScott <business@justscott.me>
2023-11-29 10:54:25 -0600

    Seperate some of the logic from promptUser into the main function. The main function now runs a loop calling the necessary functions instead of promptUser using recursion. Changed the bulk of the code to favor using pointers with structs, instead of passing and returning new structs each time. Replaced some uneccesary code, like calling piecesArray[64] instead of piecesArray[64][1], which allowed me to remove all of the extra [0]'s at the end of each piecesArray index.

--- main.c
+++ main.c
@@ -20,139 +20,160 @@
 
 
 #define TOTAL_SQUARES 64
-#define SQUARE_STR_LEN 1
-
-char playersMove[5] = "black";
 
 
 struct checkedMoveResult {
     // 0 if the function failed
     int status;
-    // {currentPosition, jumpablePosition, desiredPosition}
-    int moveInstructions[3];
+    int currentPosition;
+    int jumpablePosition;
+    int desiredPosition;
 };
 
-struct checkedMoveResult checkMove(char (*pa)[TOTAL_SQUARES][SQUARE_STR_LEN], int currentPosition, int desiredPosition) {
-    struct checkedMoveResult moveResult;
-    moveResult.status = 0;
-    moveResult.moveInstructions[0] = currentPosition;
-    moveResult.moveInstructions[2] = desiredPosition;
+struct userChosenMove {
+    int currentPosition;
+    int desiredPosition;
+};
 
+
+void checkMove(char (*pa)[TOTAL_SQUARES], struct checkedMoveResult *moveResult) {
     // Ensure the user doesn't try to move to a light square
-    if ((*pa)[desiredPosition][0] == 'l') {return moveResult;}
+    if ((*pa)[moveResult->desiredPosition] == 'l') {return;}
+
 
     // Ensure there isn't another piece in the desiredPosition
-    if ((*pa)[desiredPosition][0] != ' ') {return moveResult;}
+    if ((*pa)[moveResult->desiredPosition] != ' ') {return;}
 
     // The index of the space between the desired and current position
-    int jumpablePosition = desiredPosition - currentPosition;
-    jumpablePosition = (jumpablePosition / 2) + currentPosition;
+    int desiredMinusCurrent = moveResult->desiredPosition - moveResult->currentPosition;
+    int jumpablePosition = (desiredMinusCurrent / 2) + moveResult->currentPosition;
 
     // Ensure the piece is only moving between a 19 and 6 index change
-    int desiredMinusCurrent = desiredPosition-currentPosition;
+    //
     int absoluteDesiredMinusCurrent = desiredMinusCurrent;
-
     if (desiredMinusCurrent < 0) {
         absoluteDesiredMinusCurrent *= -1;
     }
-    if (absoluteDesiredMinusCurrent > 19) {return moveResult;}
-    if (absoluteDesiredMinusCurrent < 6) {return moveResult;}
+    if (absoluteDesiredMinusCurrent > 19) {return;}
+    if (absoluteDesiredMinusCurrent < 6) {return;}
 
 
     // if non-king white: it can only increase index
-    if ((*pa)[currentPosition][0] == 'w') {
-        if (currentPosition > desiredPosition) {return moveResult;}
+    if ((*pa)[moveResult->currentPosition] == 'w') {
+        if (moveResult->currentPosition > moveResult->desiredPosition) {return;}
 
         // if trying to jump an opponents piece
         if ((absoluteDesiredMinusCurrent) > 13) {
             // Check the char of the between piece
-            if ((*pa)[jumpablePosition][0] == 'b' || (*pa)[jumpablePosition][0] == 'B') { 
-                moveResult.moveInstructions[1] = jumpablePosition;
-            } else {return moveResult;}
+            if ((*pa)[jumpablePosition] == 'b' || (*pa)[jumpablePosition] == 'B') { 
+                moveResult->jumpablePosition = jumpablePosition;
+            } else {return;}
         }
     }
+
 
     // if non-king black: it can only decrease index
-    if ((*pa)[currentPosition][0] == 'b') {
-        if (currentPosition < desiredPosition) {return moveResult;}
+    if ((*pa)[moveResult->currentPosition] == 'b') {
+        if (moveResult->currentPosition < moveResult->desiredPosition) {return;}
 
         // if trying to jump an opponents piece
         if ((absoluteDesiredMinusCurrent) > 13) {
             // Check the char of the between piece
-            if ((*pa)[jumpablePosition][0] == 'w' || (*pa)[jumpablePosition][0] == 'W') {
-                moveResult.moveInstructions[1] = jumpablePosition;
-            } else {return moveResult;}
+            if ((*pa)[jumpablePosition] == 'w' || (*pa)[jumpablePosition] == 'W') {
+                moveResult->jumpablePosition = jumpablePosition;
+            } else {return;}
         }
     }
 
+
     //  if black or white king piece
-    if ((*pa)[currentPosition][0] == 'W' || (*pa)[currentPosition][0] == 'B') {
+    if ((*pa)[moveResult->currentPosition] == 'W' || (*pa)[moveResult->currentPosition] == 'B') {
         // If moving more than one square
         if (absoluteDesiredMinusCurrent > 13) {
             // if white king
-            if ((*pa)[currentPosition][0] == 'W') {
-                if ((*pa)[jumpablePosition][0] == 'b' || (*pa)[jumpablePosition][0] == 'B') {
+            if ((*pa)[moveResult->currentPosition] == 'W') {
+                if ((*pa)[jumpablePosition] == 'b' || (*pa)[jumpablePosition] == 'B') {
                     // if an opponents piece, remove it
-                    moveResult.moveInstructions[1] = jumpablePosition;
-                } else {return moveResult;}   
+                    moveResult->jumpablePosition = jumpablePosition;
+                } else {return;}   
             // if black king
-            } else if ((*pa)[currentPosition][0] == 'B') {
-                if ((*pa)[jumpablePosition][0] == 'w' || (*pa)[jumpablePosition][0] == 'W') {
+            } else if ((*pa)[moveResult->currentPosition] == 'B') {
+                if ((*pa)[jumpablePosition] == 'w' || (*pa)[jumpablePosition] == 'W') {
                     // if an opponents piece, remove it
-                    moveResult.moveInstructions[1] = jumpablePosition;
-                } else {return moveResult;}
+                    moveResult->jumpablePosition = jumpablePosition;
+                } else {return;}
             }
         }
     }
 
+
     // Successful logic
-    moveResult.status = 1;
-    return moveResult; 
+    moveResult->status = 1;
 }
 
-int makeMove(char (*pa)[TOTAL_SQUARES][SQUARE_STR_LEN], int currentPosition, int jumpablePosition, int desiredPosition) {
+int makeMove(char (*pa)[TOTAL_SQUARES], int currentPosition, int jumpablePosition, int desiredPosition) {
     if (jumpablePosition) {
-        (*pa)[jumpablePosition][0] = ' ';
+        (*pa)[jumpablePosition] = ' ';
     }
 
     // Move the piece out of the currentPosition and into the desiredPosition 
-    (*pa)[desiredPosition][0] = (*pa)[currentPosition][0]; 
-    (*pa)[currentPosition][0] = ' ';
+    (*pa)[desiredPosition] = (*pa)[currentPosition]; 
+    (*pa)[currentPosition] = ' ';
+
 
     // Kinging a white piece
-    if ((*pa)[56][0] == 'w') {(*pa)[56][0] = 'W';}
-    if ((*pa)[58][0] == 'w') {(*pa)[58][0] = 'W';}
-    if ((*pa)[60][0] == 'w') {(*pa)[60][0] = 'W';}
-    if ((*pa)[62][0] == 'w') {(*pa)[62][0] = 'W';}
+    if ((*pa)[56] == 'w') {(*pa)[56] = 'W';}
+    if ((*pa)[58] == 'w') {(*pa)[58] = 'W';}
+    if ((*pa)[60] == 'w') {(*pa)[60] = 'W';}
+    if ((*pa)[62] == 'w') {(*pa)[62] = 'W';}
 
     // Kinging a black piece
-    if ((*pa)[1][0] == 'b') {(*pa)[1][0] = 'B';}
-    if ((*pa)[3][0] == 'b') {(*pa)[3][0] = 'B';}
-    if ((*pa)[5][0] == 'b') {(*pa)[5][0] = 'B';}
-    if ((*pa)[7][0] == 'b') {(*pa)[7][0] = 'B';}
+    if ((*pa)[1] == 'b') {(*pa)[1] = 'B';}
+    if ((*pa)[3] == 'b') {(*pa)[3] = 'B';}
+    if ((*pa)[5] == 'b') {(*pa)[5] = 'B';}
+    if ((*pa)[7] == 'b') {(*pa)[7] = 'B';}
 }
 
-int checkMultiHop(char (*pa)[TOTAL_SQUARES][SQUARE_STR_LEN], int currentPosition) {
+int checkMultiHop(char (*pa)[TOTAL_SQUARES], int desiredPosition) {
     // Run the squares that are 14 and 18 away from the new currentPosition
     //  through checkMove to see if they are jumpable to, if they are, return
     //  1, else return 0. Further logic is handled by the other functions
-    struct checkedMoveResult moveDownRight = checkMove(pa, currentPosition, currentPosition+18);
-    struct checkedMoveResult moveUpLeft = checkMove(pa, currentPosition, currentPosition-18);
-    struct checkedMoveResult moveDownLeft = checkMove(pa, currentPosition, currentPosition+14);
-    struct checkedMoveResult moveUpRight = checkMove(pa, currentPosition, currentPosition-14);
 
-    return moveDownRight.status || moveUpLeft.status || moveDownLeft.status || moveUpRight.status;
+    struct checkedMoveResult theoreticalMoveResult;
+    theoreticalMoveResult.status = 0;
+    theoreticalMoveResult.jumpablePosition = 0;
+
+    theoreticalMoveResult.currentPosition = desiredPosition;
+    theoreticalMoveResult.desiredPosition = desiredPosition+18;
+    checkMove(pa, &theoreticalMoveResult);
+    if (theoreticalMoveResult.status) {return 1;}
+    
+    theoreticalMoveResult.currentPosition = desiredPosition;
+    theoreticalMoveResult.desiredPosition = desiredPosition-18;
+    if (theoreticalMoveResult.status) {return 1;}
+    
+    theoreticalMoveResult.currentPosition = desiredPosition;
+    theoreticalMoveResult.desiredPosition = desiredPosition+14;
+    checkMove(pa, &theoreticalMoveResult);
+    if (theoreticalMoveResult.status) {return 1;}
+    
+    theoreticalMoveResult.currentPosition = desiredPosition;
+    theoreticalMoveResult.desiredPosition = desiredPosition-14;
+    checkMove(pa, &theoreticalMoveResult);
+    if (theoreticalMoveResult.status) {return 1;}
+
+    return 0;
 }
 
-void promptUser(char (*pa)[TOTAL_SQUARES][SQUARE_STR_LEN]) {
-    int currentPosition;
-    int desiredPosition;
-
+void promptUser(char (*pa)[TOTAL_SQUARES], struct userChosenMove *user, char (*playersMove)[6]) {
     int tryLimit = 10;
 
     // Ensure the player chooses one of their pieces
-    while (tryLimit > 0) {
+    while (tryLimit--) {
         printf("\e[1;1H\e[2J"); // Clears the screen
+        if (tryLimit < 9) {
+            printf("\n\t- Invalid Choice, Try Again -\n");
+        }
         printf(
             "\n"
             "+-----+-----+-----+-----+-----+-----+-----+-----+\n"
@@ -173,102 +194,55 @@ void promptUser(char (*pa)[TOTAL_SQUARES][SQUARE_STR_LEN]) {
             "| 56%c |     | 58%c |     | 60%c |     | 62%c |     |\n"
             "+-----+-----+-----+-----+-----+-----+-----+-----+\n",
 
-            (*pa)[1][0], (*pa)[3][0], (*pa)[5][0], (*pa)[7][0],
-            (*pa)[8][0], (*pa)[10][0],(*pa)[12][0],(*pa)[14][0],
-            (*pa)[17][0],(*pa)[19][0],(*pa)[21][0],(*pa)[23][0],
-            (*pa)[24][0],(*pa)[26][0],(*pa)[28][0],(*pa)[30][0],
-            (*pa)[33][0],(*pa)[35][0],(*pa)[37][0],(*pa)[39][0],
-            (*pa)[40][0],(*pa)[42][0],(*pa)[44][0],(*pa)[46][0],
-            (*pa)[49][0],(*pa)[51][0],(*pa)[53][0],(*pa)[55][0],
-            (*pa)[56][0],(*pa)[58][0],(*pa)[60][0],(*pa)[62][0]
+            (*pa)[1], (*pa)[3], (*pa)[5], (*pa)[7],
+            (*pa)[8], (*pa)[10],(*pa)[12],(*pa)[14],
+            (*pa)[17],(*pa)[19],(*pa)[21],(*pa)[23],
+            (*pa)[24],(*pa)[26],(*pa)[28],(*pa)[30],
+            (*pa)[33],(*pa)[35],(*pa)[37],(*pa)[39],
+            (*pa)[40],(*pa)[42],(*pa)[44],(*pa)[46],
+            (*pa)[49],(*pa)[51],(*pa)[53],(*pa)[55],
+            (*pa)[56],(*pa)[58],(*pa)[60],(*pa)[62]
         );
 
-        tryLimit--;
-        printf("\n(%s) Enter the square number of the piece you wish to move: ", playersMove);
-        scanf("%i", &currentPosition);
+        printf("\n(%s) Enter the square number of the piece you wish to move: ", *playersMove);
+        scanf("%i", &user->currentPosition);
 
-        // If it's white move
-        if ((*pa)[currentPosition][0] == 'w' || (*pa)[currentPosition][0] == 'W') {
-            if (playersMove[0] == 'w') {
-                break;
-            }
-        // If it's blacks move
-        } else if ((*pa)[currentPosition][0] == 'b' || (*pa)[currentPosition][0] == 'B') {
-            if (playersMove[0] == 'b') {
-                break;
-            }
-        // If the player chooses an incorrect piece
-        } else {
-            continue;
-        }
-    }
+        // Ensure the desired position is within the board/array size to
+        //  avoid index errors
+        if (user->currentPosition < TOTAL_SQUARES-1 && user->currentPosition >= 0) {
+            // If it's white's moves
+            if ((*pa)[user->currentPosition] == 'w' || (*pa)[user->currentPosition] == 'W') {
+                // If the player choose the opponent's color
+                if ((*playersMove)[0] != 'w') {
+                    continue;
+                }
+            // If it's black's move
+            } else if ((*pa)[user->currentPosition] == 'b' || (*pa)[user->currentPosition] == 'B') {
+                // If the player choose the opponent's color
+                if ((*playersMove)[0] != 'b') {
+                    continue;
+                }
+            } else {continue;} // If the player chooses a blank or white piece
+        } else {continue;}
 
-    tryLimit = 10;
-    while (tryLimit > 0) {
-        tryLimit--;
         // Picking a square to move to
         printf(
             "\n(%s) Which square number should we move '%i%c' to: ", 
-            playersMove, currentPosition, (*pa)[currentPosition][0]
+            *playersMove, user->currentPosition, (*pa)[user->currentPosition]
         );
-        scanf("%i", &desiredPosition);
+
+        scanf("%i", &user->desiredPosition);
+
         // Ensure the desired position is within the board/array size to
         //  avoid index errors
-        if (desiredPosition < TOTAL_SQUARES-1 && desiredPosition >= 0) {break;}
+        if (user->desiredPosition < TOTAL_SQUARES-1 && user->desiredPosition >= 0) {break;}
     }
-
-
-    struct checkedMoveResult checkedMoveResult1 = checkMove(pa, currentPosition, desiredPosition);
-    
-    // If the checked move above was valid, make the move
-    if (checkedMoveResult1.status) {
-        makeMove(
-            pa, 
-            checkedMoveResult1.moveInstructions[0],
-            checkedMoveResult1.moveInstructions[1],
-            checkedMoveResult1.moveInstructions[2]
-        );
-    }
-
-
-    // Ensure the piece is only moving between a 19 and 6 index change
-    int desiredMinusCurrent = desiredPosition-currentPosition;
-    int absoluteDesiredMinusCurrent = desiredMinusCurrent;
-
-    if (desiredMinusCurrent < 0) {
-        absoluteDesiredMinusCurrent *= -1;
-    }
-
-    // To be able to skip switching players
-    //  1. The current player had to of just jumped an opponent's piece
-    //  2. Their has to be a spot open for another jump as the next move
-    if (absoluteDesiredMinusCurrent > 13 && checkMultiHop(pa, desiredPosition)) {
-        
-    } else if (checkedMoveResult1.status) {
-        // switch players
-        if (playersMove[0] == 'b') {
-            playersMove[0] = 'w';
-            playersMove[1] = 'h';
-            playersMove[2] = 'i';
-            playersMove[3] = 't';
-            playersMove[4] = 'e';
-        }
-        else {
-            playersMove[0] = 'b';
-            playersMove[1] = 'l';
-            playersMove[2] = 'a';
-            playersMove[3] = 'c';
-            playersMove[4] = 'k';
-        }
-    }
-
-    promptUser(pa);
 }
 
 void main () {
-    // Each piece will take up 3 spaces, replacing chars with empty
-    //  space when needed
-    char pieces_array[TOTAL_SQUARES][SQUARE_STR_LEN] = {
+    // 'l' = light (square the player can't move to)
+    // ' ' = open square the player can move to
+    char piecesArray[TOTAL_SQUARES] = {
         'l', 'w', 'l', 'w', 'l', 'w', 'l', 'w',
         'w', 'l', 'w', 'l', 'w', 'l', 'w', 'l',
         'l', 'w', 'l', 'w', 'l', 'w', 'l', 'w',
@@ -278,6 +252,66 @@ void main () {
         'l', 'b', 'l', 'b', 'l', 'b', 'l', 'b',
         'b', 'l', 'b', 'l', 'b', 'l', 'b', 'l'
     };
-    promptUser(&pieces_array);
+
+    char playersMove[6] = "black";
+
+    // Declared outside of the loop for readability
+    //
+    int desiredMinusCurrent;
+    int absoluteDesiredMinusCurrent;
+    struct userChosenMove user;
+    struct checkedMoveResult moveResult;
+
+    int moveLimit = 10000;
+    while (moveLimit--) {
+        promptUser(&piecesArray, &user, &playersMove);
+
+        moveResult.status = 0;
+        moveResult.currentPosition = user.currentPosition;
+        moveResult.desiredPosition = user.desiredPosition;
+        moveResult.jumpablePosition = 0;
+
+        checkMove(&piecesArray, &moveResult);
+
+        // If the checked move above was valid, make the move
+        if (moveResult.status) {
+            makeMove(
+                &piecesArray, 
+                moveResult.currentPosition,
+                moveResult.jumpablePosition,
+                moveResult.desiredPosition
+            );
+        }
+
+        // Ensure the piece is only moving between a 19 and 6 index change
+        desiredMinusCurrent = user.desiredPosition-user.currentPosition;
+        absoluteDesiredMinusCurrent = desiredMinusCurrent;
+
+        if (desiredMinusCurrent < 0) {
+            absoluteDesiredMinusCurrent *= -1;
+        }
+
+        // To be able to skip switching players
+        //  1. The current player had to of just jumped an opponent's piece
+        //  2. Their has to be a spot open for another jump as the next move
+        if (absoluteDesiredMinusCurrent > 13 && checkMultiHop(&piecesArray, user.desiredPosition)) {
+        } else if (moveResult.status) {
+            // switch players
+            if (playersMove[0] == 'b') {
+                playersMove[0] = 'w';
+                playersMove[1] = 'h';
+                playersMove[2] = 'i';
+                playersMove[3] = 't';
+                playersMove[4] = 'e';
+            }
+            else {
+                playersMove[0] = 'b';
+                playersMove[1] = 'l';
+                playersMove[2] = 'a';
+                playersMove[3] = 'c';
+                playersMove[4] = 'k';
+            }
+        }
+    }
 }
 
